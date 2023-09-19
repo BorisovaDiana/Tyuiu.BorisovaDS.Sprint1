08d8057a517e816c822c51b7bc67056f3aba4055
Tyuiu.BorisovaDS@outlook.com <yammi253525@gmail.com>
2023-09-20 00:09:35 +0300

    Add project files.

---
+++ Tyuiu.BorisovaDS.Sprint1.Task0.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BorisovaDS.Sprint1.Task0.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BorisovaDS.Sprint1.Task0.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fcca62c2-4e69-4fce-a486-8aad8f8c7e61")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
