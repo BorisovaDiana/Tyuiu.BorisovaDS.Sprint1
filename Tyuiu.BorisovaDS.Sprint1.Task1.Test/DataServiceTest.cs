a3820a798eac0bcada598c3d78955a5bf7d5449a
Tyuiu.BorisovaDS@outlook.com <yammi253525@gmail.com>
2023-09-20 19:43:21 +0300

    Task1

---
+++ Tyuiu.BorisovaDS.Sprint1.Task1.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.BorisovaDS.Sprint1.Task1.V12.Lib;
+
+namespace Tyuiu.BorisovaDS.Sprint1.Task1.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 3.0;
+            double y = 3.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
