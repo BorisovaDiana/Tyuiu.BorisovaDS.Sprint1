34736311c4e632dceee8209f67509db3bf96a12b
Tyuiu.BorisovaDS@outlook.com <yammi253525@gmail.com>
2023-09-29 00:48:08 +0300

    Task6

---
+++ Tyuiu.BorisovaDS.Sprint1.Task6.V18.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.BorisovaDS.Sprint1.Task6.V18.Lib
+{
+    public class DataService : ISprint1Task6V18
+    {
+
+        public bool CheckNumber(string value)
+        {
+            foreach (char c in value)
+                if (Char.IsNumber(c))
+                {
+                    return true;
+                }
+
+                else
+                {
+                    return false;
+                }
+
+        }
+                
+    }
+}
---
+++ Tyuiu.BorisovaDS.Sprint1.Task6.V18/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BorisovaDS.Sprint1.Task6.V18.Lib;
+
+namespace Tyuiu.BorisovaDS.Sprint1.Task6.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Борисова Д. С. | ИСТН-б-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #18                                                              *");
+            Console.WriteLine("* Выполнила: Борисова Диана Сергеевна | ИСТН-б-23-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу: пользователь вводит текст. Проверить, что строка    *");
+            Console.WriteLine("* является символьным представлением натурального числа                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите текст: ");
+            string x = Console.ReadLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            bool res = ds.CheckNumber(x);
+            if (res == true)
+            {
+                Console.WriteLine("Натуральное число");
+            }
+            if (res == false)
+            {
+                Console.WriteLine("Не натуральное число");
+            }
+            Console.ReadLine();
+        }
+    }
+}
